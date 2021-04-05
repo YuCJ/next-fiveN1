df2a9c023128e50bb7249f5690b9697316af013b
yucj <nakts0123@gmail.com>
2021-04-05 23:51:34 +0800

    chore: remove tracking

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
 const API_URL = process.env.API_URL || 'http://localhost:8000';
-const TRACKING_ID = process.env.TRACKING_ID || 'development';
 
 module.exports = {
   target: 'serverless',
   env: {
     API_URL,
-    TRACKING_ID
   }
 };
--- pages/_document.js
+++ pages/_document.js
@@ -13,22 +13,7 @@ export default class extends Document {
   render() {
     return (
       <html>
-        <Head>
-          <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.TRACKING_ID}`}
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.TRACKING_ID}');
-          `
-            }}
-          />
-        </Head>
+        <Head />
         <body>
           <Main />
           <NextScript />
